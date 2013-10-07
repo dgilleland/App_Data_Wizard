a5e2772770d97d55a69309c615b272cbec155a67
Dan Gilleland <dgilleland@nait.ca>
2013-10-07 11:38:11 -0600

    Quick modifications, also non-compiling

--- src/Website/App_Data_Wizard/DbScriptWizard.ascx
+++ src/Website/App_Data_Wizard/DbScriptWizard.ascx
@@ -1,1 +1,49 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="DbScriptWizard.ascx.cs" Inherits="App_Data_Wizard_DbScriptWizard" %>
+    <div>
+    
+        <h2>
+            Database Configuration &amp; Setup</h2>
+        <p>
+            The expected connection string name is
+            <asp:Label ID="ConnectionStringName" runat="server" Font-Bold="True" 
+                ForeColor="Blue" Text="OMST_db"></asp:Label>
+            .</p>
+        <p>
+            The actual connection string value is
+            <asp:Label ID="ConnectionStringDetails" runat="server" Font-Bold="True" 
+                Font-Italic="True" Font-Overline="True" Font-Underline="True" 
+                ForeColor="#009933"></asp:Label>
+            .</p>
+        <p>
+            The database content was<asp:Label ID="NotAccessible" 
+                runat="server" Font-Italic="True" Text="&amp;nbsp;not" Visible="False"></asp:Label>&nbsp;accessible.&nbsp;</p>
+        <p>
+            <asp:Label ID="MessageLabel" runat="server" Font-Bold="True" 
+        ForeColor="#CC0000"></asp:Label>
+</p>
+        <asp:Panel ID="DatabaseRebuildPanel" runat="server">
+            <h4>
+                Database Rebuild Details</h4>
+            <p>
+                Scripts Run:</p>
+            <p>
+                <asp:GridView ID="ScriptsRunGridview" runat="server" 
+                    onrowdatabound="ScriptsRunGridview_RowDataBound">
+                </asp:GridView>
+            </p>
+        </asp:Panel>
+    
+    </div>
+<asp:Wizard ID="Wizard1" runat="server">
+    <WizardSteps>
+        <asp:WizardStep runat="server" title="Database Connection">
+        </asp:WizardStep>
+        <asp:WizardStep runat="server" title="Database Objects">
+        </asp:WizardStep>
+        <asp:WizardStep runat="server" Title="Available Scripts">
+        </asp:WizardStep>
+        <asp:WizardStep runat="server" Title="Execution Plan">
+        </asp:WizardStep>
+    </WizardSteps>
+</asp:Wizard>
+
--- src/Website/App_Data_Wizard/DbScriptWizard.ascx.cs
+++ src/Website/App_Data_Wizard/DbScriptWizard.ascx.cs
@@ -14,8 +14,137 @@ public partial class App_Data_Wizard_DbScriptWizard : System.Web.UI.UserControl
     {
 
     }
+    #region Private Fields
+    //// Following line adapted from the DotNetNuke.Data.SqlDataProvider SqlDelimiterRegex property
+    //private static Regex SqlDelimiterRegex = new Regex(@"(?<=(?:[^\w]+|^))GO(?=(?: |\t)*?(?:\r?\n|$))", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Multiline);
+    //private Database _DataStore = null;
+    //public Database DataStore
+    //{
+    //    get
+    //    {
+    //        if (_DataStore == null)
+    //            try
+    //            {
+    //                _DataStore = DatabaseFactory.CreateDatabase(ConnectionStringName.Text);
+    //            }
+    //            catch (ConfigurationErrorsException ex)
+    //            {
+    //                ConnectionStringDetails.Text = "Missing: " + ex.Message;
+    //                ConnectionStringDetails.Text += "<blockquote>(Falling back to hard-coded connection string details)</blockquote>";
+    //                try
+    //                {
+    //                    _DataStore = new SqlDatabase(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|OMST_Database.mdf;Integrated Security=True;User Instance=True");
+    //                }
+    //                catch (SqlException ex2)
+    //                {
+    //                    ConnectionStringDetails.Text += "<p>Fallback connection failed: " + ex2.Message + "</p>";
+    //                }
+    //            }               
+    //        return _DataStore;
+    //    }
+    //}
+    //public string ConnectionString
+    //{
+    //    get
+    //    {
+    //        return DataStore.ConnectionString;
+    //    }
+    //}
+    #endregion
+
+    #region Event Handlers
+    //protected void Page_Load(object sender, EventArgs e)
+    //{
+    //    if (!Page.IsPostBack)
+    //    {
+    //        try
+    //        {
+    //            ConnectionStringDetails.Text = ConnectionString;
+    //            MessageLabel.Text = "";
+    //            using (IDataReader reader = DataStore.ExecuteReader(CommandType.Text, "SELECT * FROM Movie"))
+    //            {
+    //                MessageLabel.Text = "Able to read from Movie table.<br/>";
+    //            }
+    //            using (IDataReader reader = DataStore.ExecuteReader(CommandType.Text, "SELECT * FROM Purchase"))
+    //            {
+    //                MessageLabel.Text += "Able to read from Purchase table.<br/>";
+    //            }
+    //            using (IDataReader reader = DataStore.ExecuteReader(CommandType.Text, "SELECT * FROM ShowTime"))
+    //            {
+    //                MessageLabel.Text += "Able to read from ShowTime table.<br/>";
+    //            }
+    //            NotAccessible.Visible = false;
+    //            DatabaseRebuildPanel.Visible = false;
+    //        }
+    //        catch (SqlException ex)
+    //        {
+    //            NotAccessible.Visible = true;
+    //            DatabaseRebuildPanel.Visible = true;
+    //            RunInstallationScript();
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            NotAccessible.Visible = true;
+    //            DatabaseRebuildPanel.Visible = false;
+    //            MessageLabel.Text = "Serious error: cannot continue. <blockquote>" + ex.Message + "<blockquote>" + ex.StackTrace + "</blockquote></blockquote>";
+    //        }
+    //    }
+    //}
+    #endregion
+
+    #region Private Methods (Script Installation)
+    //private void RunInstallationScript()
+    //{
+    //    try
+    //    {
+    //        string scriptPath = Server.MapPath("App_Data/OMST_Database_Install.sql");
+    //        string script = File.ReadAllText(scriptPath);
+    //        string[] scripts = SqlDelimiterRegex.Split(script);
+    //        List<RunnableScript> results = new List<RunnableScript>();
+    //        foreach (string item in scripts)
+    //        {
+    //            RunnableScript thisScript = new RunnableScript();
+    //            thisScript.SQLScript = item.Replace("\r\n", "<br/>");
+    //            try
+    //            {
+    //                DataStore.ExecuteNonQuery(CommandType.Text, item);
+    //                thisScript.Installed = true;
+    //            }
+    //            catch (Exception ex)
+    //            {
+    //                thisScript.Installed = false;
+    //                thisScript.FailureMessage = ex.Message.Replace("\r\n", "<br/>");
+    //            }
+    //            results.Add(thisScript);
+    //        }
+    //        ScriptsRunGridview.DataSource = results;
+    //        ScriptsRunGridview.DataBind();
+    //        MessageLabel.Text = "Script to create database items has completed successfully.";
+    //    }
+    //    catch (SqlException ex)
+    //    {
+    //        MessageLabel.Text = ex.Message + "<br/><blockquote><h5>Stack Trace</h5>" + ex.StackTrace + "</blockquote>";
+    //    }
+    //}
+    #endregion
+
+    protected void ScriptsRunGridview_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            e.Row.Cells[0].Text = Server.HtmlDecode(e.Row.Cells[0].Text);
+            e.Row.Cells[2].Text = Server.HtmlDecode(e.Row.Cells[2].Text);
+        }
+    }
 
     #region Inner Classes
+    public class RunnableScript
+    {
+        public string SQLScript { get; set; }
+        public bool Installed { get; set; }
+        public string FailureMessage { get; set; }
+    }
+
     #region Copyright
     // 
     // DotNetNuke® - http://www.dotnetnuke.com
